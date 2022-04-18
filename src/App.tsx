61ec90d4b41f9c02ac51a3769eb09f276bad67c3
didoshotev <deyan.shotev@ulpia.tech>
2022-04-18 16:36:56 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { LoginPage } from './pages/LoginPage/LoginPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppProps = { 
+	
 }
 
-export default App;
+
+export const App: React.FC<AppProps> = (): JSX.Element => {
+	
+	return (
+		<>
+			<LoginPage />		
+			Welcome!
+		</>
+	)
+}
---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+
+interface LoginPageProps {
+
+}
+
+export const LoginPage: React.FC<LoginPageProps> = (): JSX.Element => {
+    return (
+        <>
+            <img src="images/login-banner.png" alt="login-banner" />
+        </>
+    )
+}
